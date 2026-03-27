2081c466bb1be6a9b586300c7515a26f828996a9
tlkt7 <i.tlkt7@gmail.com>
2026-03-27 10:47:52 +0500

    Fix: use cookie sessions, array cache for serverless

--- api/index.php
+++ api/index.php
@@ -1,7 +1,11 @@
 <?php
 
-// Force serverless-compatible settings before Laravel boots
 putenv('LOG_CHANNEL=stderr');
+putenv('SESSION_DRIVER=cookie');
+putenv('CACHE_DRIVER=array');
+
 $_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['CACHE_DRIVER'] = 'array';
 
 require __DIR__ . '/../public/index.php';
