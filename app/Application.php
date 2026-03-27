d0fd2df7c10533705145dfa05910edc24d1bcdec
tlkt7 <i.tlkt7@gmail.com>
2026-03-27 10:44:23 +0500

    Fix: override storagePath for serverless, pre-compile views

--- app/Application.php
+++ app/Application.php
@@ -25,4 +25,33 @@ class Application extends \Illuminate\Foundation\Application
 
         return $tmpDir . ($path ? DIRECTORY_SEPARATOR . $path : $path);
     }
+
+    public function storagePath($path = '')
+    {
+        $tmpStorage = '/tmp/laravel-storage';
+
+        if (!is_dir($tmpStorage . '/framework/views')) {
+            mkdir($tmpStorage . '/framework/views', 0755, true);
+        }
+        if (!is_dir($tmpStorage . '/framework/cache/data')) {
+            mkdir($tmpStorage . '/framework/cache/data', 0755, true);
+        }
+        if (!is_dir($tmpStorage . '/logs')) {
+            mkdir($tmpStorage . '/logs', 0755, true);
+        }
+
+        // Copy pre-compiled views to /tmp
+        $sourceViews = dirname(__DIR__) . '/storage/framework/views';
+        $destViews = $tmpStorage . '/framework/views';
+        if (is_dir($sourceViews)) {
+            foreach (glob($sourceViews . '/*.php') as $file) {
+                $dest = $destViews . '/' . basename($file);
+                if (!file_exists($dest)) {
+                    copy($file, $dest);
+                }
+            }
+        }
+
+        return $tmpStorage . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
 }
