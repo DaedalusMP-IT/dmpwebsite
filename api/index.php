f6b1ad12a0a8651464b23424dede37b395869acc
tlkt7 <i.tlkt7@gmail.com>
2026-03-27 10:32:42 +0500

    Fix: force stderr logging for serverless

--- api/index.php
+++ api/index.php
@@ -1,5 +1,13 @@
 <?php
 
+// Force serverless-compatible settings before Laravel boots
+putenv('LOG_CHANNEL=stderr');
+$_ENV['LOG_CHANNEL'] = 'stderr';
+putenv('CACHE_DRIVER=array');
+$_ENV['CACHE_DRIVER'] = 'array';
+putenv('SESSION_DRIVER=cookie');
+$_ENV['SESSION_DRIVER'] = 'cookie';
+
 // Use /tmp for writable paths in serverless environment
 $tmpPath = '/tmp/storage';
 if (!is_dir($tmpPath)) {
@@ -11,5 +19,6 @@ if (!is_dir($tmpPath)) {
 }
 
 $_ENV['APP_STORAGE'] = $tmpPath;
+putenv('APP_STORAGE=' . $tmpPath);
 
 require __DIR__ . '/../public/index.php';
